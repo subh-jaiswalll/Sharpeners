aa020f61ac5d21e68f2f053743e309cf0358a26a
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-07-27 22:29:14 +0530

    Middle ware done

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const app = express();
 
 const PORT  = 3000;
 
+app.use((req, res, next) => {
+    console.log("Book Library");
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log("Book Recomdation");
+    next();
+})
+
+app.use('/library1', (req, res, next) => {
+    res.send("<h1> Library 1 </h1>");
+    next();
+})
+
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
