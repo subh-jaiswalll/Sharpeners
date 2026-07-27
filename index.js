08481608759953389e40c568026f5295f742a971
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-07-27 23:02:03 +0530

    middle ware logging

--- index.js
+++ index.js
@@ -21,38 +21,6 @@ const PORT  = 3000;
 // })
 
 
-// Set up a dynamic GET route:
-
-// Create a route in your Express app with a route parameter in the URL.
-
-// Example: /welcome/:username
-
-// This allows the app to extract the username directly from the URL.
-
-// Use a query parameter for additional info:
-
-// Accept a query parameter (like role) in the same request URL.
-
-// Example: /welcome/Julian?role=Admin
-
-// This allows the app to extract extra data from the query string.
-
-// Extract values inside your route handler:
-
-// Use Express methods to retrieve the route parameter and query parameter.
-
-// Use them to customize your response.
-
-// Send a personalized response:
-
-// Send an response with a message that uses both the username and role.
-
-// For example: Welcome Julian, your role is Admin
-
-// Test your route using Postman and push the code to Github:
-
-// Try different combinations of route and query parameters in Postman to make sure your app handles them correctly.
-
 
 app.get("/welcome/:name", (req, res) => {
     const name = req.params.name;
@@ -60,22 +28,25 @@ app.get("/welcome/:name", (req, res) => {
     res.send(`Welcome ${name}, your role is ${role}`)
 })
 
-// app.post('/products', (req, res) => {
+app.get('/products', (req, res) => {
+    res.send('Here is the list of all products...')
+})
+app.post('/products', (req, res) => {
 
-//     res.send(' A new product has been created...')
-// })
+    res.send(' A new product has been created...')
+})
 
-// app.get('/categories', (req, res) => {
-//     res.send('Here is the list of all categories...')
-// })
+app.get('/categories', (req, res) => {
+    res.send('Here is the list of all categories...')
+})
 
-// app.post('/categories', (req, res) => {
-//     res.send('A new categories has been created...')
-// })
-
-// app.use((req, res) => {
-//     res.status(404).send('<h1>404 - Page Not Found</h1>');
-// });
+app.post('/categories', (req, res) => {
+    res.send('A new categories has been created...')
+})
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
+});
 
 
 app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}`))
